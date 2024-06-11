01bf55e0c766c73b9030b5bbceb0ca7b695c008b
Evaldo Klock <neto.nemesis@gmail.com>
2024-06-11 15:16:32 -0400

    fix

--- cpf_regform/forms.py
+++ cpf_regform/forms.py
@@ -5,22 +5,21 @@ from .models import ExtraInfo
 
 
 class ExtraInfoForm(ModelForm):
+    cpf = BRCPFField()
 
-    cpf = BRCPFField
     """
     The fields on this form are derived from the ExtraInfo model in models.py.
     """
     def __init__(self, *args, **kwargs):
         super(ExtraInfoForm, self).__init__(*args, **kwargs)
-        self.fields['cpf'].required=True
+        self.fields['cpf'].required = True
 
-    class Meta(object):
-       model = ExtraInfo
-       fields = ('cpf')
-       labels = {
-           'cpf': 'CPF',
-       }
-       help_text={
-           'cpf': 'Insira seu CPF',
-       }
-    
+    class Meta:
+        model = ExtraInfo
+        fields = ('cpf',)  # Ensure this is a tuple, even for a single field
+        labels = {
+            'cpf': 'CPF',
+        }
+        help_texts = {
+            'cpf': 'Insira seu CPF',
+        }
